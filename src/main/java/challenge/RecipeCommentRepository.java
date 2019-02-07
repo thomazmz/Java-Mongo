311d529219f852ff0884735f257f6bd8a2b55781
thomazmz <thomaz.zandonotto@gmail.com>
2019-02-07 17:04:48 -0200

    Refatoração de todos os serviços

--- src/main/java/challenge/RecipeCommentRepository.java
+++
@@ -1,6 +0,0 @@
-package challenge;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RecipeCommentRepository extends MongoRepository<RecipeComment, String> {
-}
